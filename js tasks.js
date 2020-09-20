f522526d0fa0dd5c804573d20201ba4ef27bd7bc
Tymur <69144419+Ninjaas111@users.noreply.github.com>
2020-09-20 12:21:28 +0300

    і

--- js tasks.js
+++ js tasks.js
@@ -67,3 +67,56 @@ function reverseArray(arr) {
 return arr2;
 }
 console.log(reverseArray(arr1));
+
+//5
+let arr1=[];
+let arr2=[];
+
+function canNest(arr1, arr2){
+        let a=Math.min(...arr1);
+        let b=Math.min(...arr2);
+        let c=Math.max(...arr1);
+        let d=Math.max(...arr2);
+   if (a > b && c < d) {
+    return true;
+}else {
+    return false;
+} 
+}
+canNest([1,2,3,4], [0,6]);      
+
+//6 
+let element;
+        let arr2=[];
+
+        function evenNumbers(arr){
+
+               
+        for (let i=0; i < arr.length; i++){
+            element=arr[i];
+            if (element % 2 == 0 ){
+                arr2.push(element);
+            }
+        }
+    } 
+    evenNumbers([1,5,6,8,8,9,12,15,16]);
+    alert(arr2);
+
+    //7
+
+
+    function sliceSum(arr, n) {
+        let sum=0;
+        let arr2=arr.slice([0], [n]);
+        for (let i=0; i<arr2.length; i++){
+        sum +=arr2[i];
+}
+        return sum;
+    }
+    sliceSum([1, 3, 2], 2);
+
+
+    //8
+    function sortByLength(arr) {
+        return arr.sort((a,b) => a.length - b.length );
+    }
